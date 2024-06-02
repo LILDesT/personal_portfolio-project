d7c4af757709107c39ca4c8c241c515c00d144fe
LILDesT <145715407+LILDesT@users.noreply.github.com>
2024-06-02 17:43:10 +0300

    Add files via upload

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Blog
 
-# Create your views here.
+def all_blogs(request):
+    projects1 = Blog.objects.order_by('-Data')[:5]
+    return render(request, 'blog/all_blogs.html', {'projects1':projects1})
+
+def detail(request, blog_id):
+    blog = get_object_or_404(Blog, pk=blog_id)
+    return render(request, 'blog/detail.html',{'blog':blog})
+
--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "blog",
+    "portfolio",
 ]
 
 MIDDLEWARE = [
@@ -115,3 +117,6 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+MEDIA_ROOT =  BASE_DIR / "Media"
+MEDIA_URL = "/Media/"
