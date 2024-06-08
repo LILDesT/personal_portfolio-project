f335d0d12eac032e54a673162dd30ab08682006f
shamil DEST <shamiltron@yandex.com>
2024-06-08 19:17:38 +0300

    UPD

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Blog(models.Model):
+    title = models.CharField(max_length=200)
+    Data = models.DateField()
+    description = models.TextField(max_length=250)
+
+    def __str__(self):
+        return self.title
+
+
+
